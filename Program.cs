60634a6d35f824b0a6de69009e89e9b3e34a4f39
Nifemi <rashid.oluwaseun@yahoo.com>
2021-11-05 09:18:52 -0700

    Initialized project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Matrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrixA = {
+                {   1, 2, 4, 5  },
+                {   2, 5, 22, 3  },
+                {   3, 2, 8, 6  },
+                {   4, 0, 3, 9  },
+            };
+
+            //int[,] matrixB = {
+            //    {   0, 7, 2, 40  },
+            //    {   5, 9, 1, 2  },
+            //    {   6, 23, 4, 6  },
+            //    {   3, 4, 10, 5  },
+            //};
+
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
+        }
+    }
+}
